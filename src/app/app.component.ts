76235fc6f6509738e624a6b83d71b7c7c66d02e1
matthias <matteutsch@gmail.com>
2024-02-07 16:21:48 +0100

    +customized (non-clickable) scrollbar; + bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ScrollWatcherComponent } from './scroll-watcher/scroll-watcher.component';
+import { CustomScrollbarComponent } from './custom-scrollbar/custom-scrollbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ScrollWatcherComponent],
+  imports: [RouterOutlet, ScrollWatcherComponent, CustomScrollbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/custom-scrollbar/custom-scrollbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-scrollbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-scrollbar.component.html',
+  styleUrl: './custom-scrollbar.component.scss'
+})
+export class CustomScrollbarComponent {
+
+}
